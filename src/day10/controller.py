4c17c146036076a44b3e4b67433c86e3842a5fbd
dlwoauddlqslek <ljm5857@gmail.com>
2024-08-27 20:13:58 +0900

    쿠우쿠우

--- src/day10/controller.py
+++ src/day10/controller.py
@@ -3,16 +3,16 @@ from app import app # Flask 객체 호출
 @app.route("/qooqoo", methods=["GET"])
 def load() :
     list = []
-    f = open("qooqoo2.csv", "r")  # 파일 읽기 모드
+    f = open("qooqoo4.csv", "r")  # 파일 읽기 모드
     next(f)  # 첫번째 줄 스킵
     readlines = f.read()  # 파일 읽기
     rows = readlines.split("\n")  # 행 구분해서 저장
     for i in rows :
         if i :
             cols = i.split(',')             # 쉼표 구분해서 저장
-            if len(cols)==6:
-                dic = {'번호':cols[0],'매장명':cols[1],'연락처':cols[2],'주소':cols[3],'영업시간':cols[4]+cols[5]}
-                list.append(dic)
-                print(list)
+
+            dic = {'번호':cols[0],'매장명':cols[1],'연락처':cols[2],'주소':cols[3],'영업시간':cols[4]}
+            list.append(dic)
+            print(list)
     return list
 
